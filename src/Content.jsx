adb3d58c08ab93c678e8c56256f15a7064e2c00c
simonsajjizoft <simon.sajimathews@zoftsolutions.com>
2023-03-16 18:10:01 +0530

    first commit

---
+++ src/Content.jsx
@@ -0,0 +1,28 @@
+function Content() {
+    return (
+        <div className="App">
+            <h1>Simon Mathews</h1>
+            <p>Software Developer at Zoftsolutions pvt. Ltd</p>
+            <a href="https://in.linkedin.com/" style={{ marginRight: 5 + 'px' }}>Linkedin</a>
+            <a href="https://github.com/">Github</a>
+            <hr />
+            <h2>Experience</h2>
+            <h3>Software developer at Zoftsolutions</h3>
+            <p>
+                I work with Zoftsolutions in GS1 Projects, handling multiple projects
+                <br />
+                Have worked with Map using google services Directions API and distance matrix API. Worked around to create a stable company website.
+            </p>
+            <hr />
+            <h2>Skills</h2>
+            <ul>
+                <li>Angular</li>
+                <li>JavaScript</li>
+                <li>React</li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default Content;
